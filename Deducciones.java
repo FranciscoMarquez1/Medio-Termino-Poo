bf27bddb18981320fe5ff2570e690da60fcccafd
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2017-03-29 19:04:38 -0600

    Error nombre atributo.

--- Deducciones.java
+++ Deducciones.java
@@ -6,7 +6,7 @@ public class Deducciones {
 				   primaVacacionalGravada,
 				   totalIngresosGravados,
 				   maximoDeducirColegiatura,
-				   totalDeduccionesSnR,
+				   totalDeduccionesSinRetiros,
 				   deduccionesPermitidas,
 				   montoSobreElCualSeCalculaISR,
 				   cuotaFija,
